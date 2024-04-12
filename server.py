71802d7b6c9907a4698c068785c55404f2d030b4
Nguyen Tran <nguyentran@Nguyens-MacBook-Pro.local>
2024-04-12 17:56:11 -0400

    Add sort feature to score board

--- server.py
+++ server.py
@@ -50,7 +50,9 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
+            break
 
+    scoreboard = sorted(scoreboard, key=lambda k: k['score'], reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
